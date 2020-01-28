e92355b121b8e77ff6bf98cba0bc605b651c7f27
borten-fb <60380405+borten-fb@users.noreply.github.com>
2020-01-28 15:21:45 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         "tqdm>=4.33.0,<5.0.0",
         "scikit-learn>=0.20.2,<0.22.0",
         "networkx>=2.2,<2.4",
-        "tensorboardX>=1.6,<2.0",
     ],
     python_requires=">=3.6",
     keywords="machine-learning ai weak-supervision",
